1481a6a596b87752360e0fd61d4ce2a72ff73327
Chris Howe-Jones <chris.howejones@gmail.com>
2015-11-27 15:24:30 +0000

    Updated to cascading-hadoop2-mr1 2.7.1

--- project.clj
+++ project.clj
@@ -7,10 +7,13 @@
                  [org.clojure/tools.cli "0.3.3"]
                  [org.clojure/data.csv "0.1.3"]
                  [clj-time "0.11.0"]
-                 [cascalog "2.1.1"]]
+                 [cascalog "2.1.1"
+                  :exclusions [cascading/cascading-hadoop]]
+                 [cascading/cascading-hadoop2-mr1 "2.7.1"]]
   :main hbase-metrics.core
   :aot [:all]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :provided {:dependencies [[org.apache.hadoop/hadoop-client "2.7.1"]]}
-             :dev {:dependencies [[org.apache.hadoop/hadoop-client "2.7.1"]]}})
+             :dev {:dependencies [[org.apache.hadoop/hadoop-client "2.7.1"]]}}
+  :repositories [["conjars.org" "http://conjars.org/repo"]])
